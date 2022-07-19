4c1a28ddeacfc4629de0b2c3afdf7b892d2c5e2b
Takashi-sys <hd.shimizu777@icloud.com>
2022-07-19 15:10:18 +0900

    Added comment create

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Comment  # Commentを追加
 
 admin.site.register(Post)
+admin.site.register(Comment) # Commentを追加
 
 # Register your models here.
--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,15 @@
 from django import forms
-
-from .models import Post
+from .models import Post, Comment# コメントを追加
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+#追加 読者がコメントを書けるように
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
